b6d87abdd6c87cf279bf6b6279652600de00b371
sinke237 <enowsinke@gmail.com>
2024-02-25 16:35:41 +0100

    Mr Armand(Calculator): terminal app, added main Op

---
+++ playWithJava/calculator/Main.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+/**
+ * Main
+ */
+public class Main {
+    private static void getUsrOption(){
+        System.out.println("\n"+"Choose an operation...");
+        System.out.println("\n"+"1. Addition");
+        System.out.println("\n"+"2. Subtraction");
+        System.out.println("\n"+"3. Division");
+        System.out.println("\n"+"4. Multiiplication");
+
+        Scanner choice = Calculator.input;
+
+        switch (choice.nextInt()) {
+            case 1:
+                int sum = Calculator.addition();
+                System.out.println("\n"+"The sum is: "+ sum);
+                break;
+            
+            case 2:    
+                int difference = Calculator.subtraction();
+                System.out.println("\n"+"The difference is: "+difference);
+                break;
+
+            case 3:
+                double quotient = Calculator.division();
+                System.out.println("\n"+"The quotient is: "+quotient);
+                break;
+
+            case 4:
+                double product = Calculator.multiplication();
+                System.out.println("\n"+"The product is: "+product);
+                break;
+
+            default:
+                System.out.println("\n"+"Invalid option");
+                break;
+        }
+    }
+    public static void main(String[] args) {
+        getUsrOption();
+    }
+}
