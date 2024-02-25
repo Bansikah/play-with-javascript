75a7fae7f17c8677eddb8e4cf4e651b2c12d21a5
sinke237 <enowsinke@gmail.com>
2024-02-25 13:10:48 +0100

    Mr Armand(Calculator): terminal app, added divide() Op

---
+++ playWithJava/calculator/Calculator.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Calculator{
+    public static Scanner input = new Scanner(System.in);
+    private static int[] numbers;
+
+    private static void getInputs(){
+        System.out.println("\n"+"Enter two numbers");
+        int a = input.nextInt();
+        int b = input.nextInt();   
+        numbers = new int[]{a,b};                                                                                                                                                                                                                       
+    }
+
+    public static int addition(){
+        getInputs();
+        int sum = numbers[0] + numbers[1];
+
+        return sum;
+    }
+
+
+    public static int subtraction(){
+        getInputs();
+        int minus = numbers[1] - numbers[0];
+
+        return minus;
+    }
+
+    public static double division(){
+        getInputs();
+        int divide = numbers[1] / numbers[0]; 
+    }
+}
